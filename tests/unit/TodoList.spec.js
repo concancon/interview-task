5bd34a4b46618e09dfe8948f5e7d335549b8ca32
Camilo Ocampo <camiloocampo@Camilos-MacBook-Pro.local>
2020-03-29 16:06:31 +0200

    Add checkbox tests

--- tests/unit/TodoList.spec.js
+++ tests/unit/TodoList.spec.js
@@ -19,11 +19,11 @@ describe('TodoList',  () => {
 
 
 describe('TodoList',  () => {
-  it("finish task method as fired by clicking checkbox removes the element with id= 0 from unfinished todos", async () => {
+  it("finish task method as fired by clicking checkbox removes the element with id= 0 from unfinished todos",  () => {
     const wrapper = mount(TodoList)
     let FirstListItem= wrapper.find(ListItem); 
      FirstListItem.vm.$emit('finishTask', 0);
-     await Vue.nextTick();
+    //  await Vue.nextTick();
     const result=  wrapper.vm.todos.filter(obj => {
         return obj.id===0;
      })
@@ -31,3 +31,23 @@ describe('TodoList',  () => {
 
   })
 })
+
+
+
+describe('TodoList',  () => {
+    it("calling finish item on an item in finishedTasks moves it back to unfinished list", () => {
+      const wrapper = mount(TodoList)
+      const listItems=  wrapper.findAll(ListItem)
+      //given a static markup lets call finish task on an already finished task
+      listItems.at(4).vm.$emit('finishTask', 4);
+    
+      const found =  wrapper.vm.todos.filter(obj => {
+        return obj.id===4;
+     })
+      expect(found==Object); 
+    })
+  })
+
+
+
+  //TODO: TEST THAT ALL OBJECTS IN BOTH THE TODO LIST AND THE FINISHED TASKS LISTS ARE RENDERED IN THEIR ENTIRETY, TEST TEXT INPUT, TEST X FUNCTIONALITY
